4c4a97df1a131eb57e4a2756879a544f9572a467
Antonio Hernández <antoniohernandez95@gmail.com>
2020-05-18 04:18:42 -0400

    Agregados estilos al Badge

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
 
+import "./styles/Badge.css";
+import confLogo from "../images/badge-header.svg";
+
+//function Badge () {}
+//const Badge = () => {}
+
 class Badge extends Component {
   render() {
     return (
-      <div>
-        <h1>Bonjour de React</h1>
+      <div className="Badge">
+        <div className="Badge__header">
+          <img src={confLogo} alt="Logo de la Conf" />
+        </div>
+
+        <div className="Badge__section-name">
+          <img
+            className="Badge__avatar"
+            src="https://www.gravatar.com/avatar?d=identicon"
+            alt="avatar"
+          />
+          <h1>
+            Richard <br /> Kaufman
+          </h1>
+        </div>
+
+        <div className="Badge__section-info">
+          <h3>Frontend Engineer</h3>
+          <div>@Sparragus</div>
+        </div>
+
+        <div className="Badge__footer">#platziconf</div>
       </div>
     );
   }

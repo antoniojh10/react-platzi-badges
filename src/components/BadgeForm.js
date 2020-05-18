7edd4c30b35ec72d13bf2753fec4a78c97c63300
Antonio Hernández <antoniohernandez95@gmail.com>
2020-05-18 05:32:46 -0400

    Controlando inputs y trabajando con State

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 
 class BadgeForm extends Component {
+  state = {};
   handleChange = (e) => {
-    console.log(e.target.value);
+    /* console.log(e.target.value); */
+    /* this.setState({
+      firstName: e.target.value,
+    }); */
+    // Se espera que cada uno de los espacios, guarde la informacion en su propio espacio, para eso:
+
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
   };
 
   handleClick = (e) => {
@@ -11,6 +20,7 @@ class BadgeForm extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
+    console.log(this.state);
   };
 
   render() {
@@ -26,6 +36,39 @@ class BadgeForm extends Component {
               className="form-control"
               type="text"
               name="firstName"
+              value={this.state.firstName}
+            />
+            <label>Last Name</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="lastName"
+              value={this.state.lastName}
+            />
+            <label>Email</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="email"
+              name="email"
+              value={this.state.email}
+            />
+            <label>Job Title</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="jobTitle"
+              value={this.state.jobTitle}
+            />
+            <label>Twitter</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="twitter"
+              value={this.state.twitter}
             />
           </div>
           <button onClick={this.handleClick} className="btn btn-primary">

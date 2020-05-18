3d6596c2bef4bca548a001ec35fb4c4a80ff7588
Antonio Hernández <antoniohernandez95@gmail.com>
2020-05-17 20:54:35 -0400

    Primer componente

---
+++ src/components/Badge.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Badge extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Bonjour de React</h1>
+      </div>
+    );
+  }
+}
+
+export default Badge;
